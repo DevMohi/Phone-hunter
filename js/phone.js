bc591359f814b045db757ef6050ff4d5d2431c70
DevMohi <morphex72@gmail.com>
2022-03-01 01:07:29 +0600

    details loaded and showed in ui

--- js/phone.js
+++ js/phone.js
@@ -26,8 +26,8 @@ const displayData = data => {
     }
 
     else {
-        data.slice(0,20).forEach(phone => {
-            
+        data.slice(0, 20).forEach(phone => {
+
             const div = document.createElement('div')
             div.classList.add('col')
             div.innerHTML = `
@@ -38,7 +38,7 @@ const displayData = data => {
                 <h5 class="card-title text-center">${phone.phone_name}</h5>
 
                 <div class="d-flex justify-content-center border-none">
-                    <button class="border btn-danger py-2 px-3 rounded">Details</button>
+                    <button class="border btn-danger py-2 px-3 rounded" onclick="loadDetail('${phone.slug}')">Details</button>
                 </div>
 
                 </div>
@@ -51,6 +51,41 @@ const displayData = data => {
     }
 
 }
+
+// getting the id and loading it 
+const loadDetail = id => {
+    url = `https://openapi.programming-hero.com/api/phone/${id}`
+
+    fetch(url)
+        .then(res => res.json())
+        .then(details => displayDetails(details.data))
+}
+
+// showing details in ui 
+const displayDetails = data => {
+    console.log(data)
+    const parent = document.getElementById('detailsSection')
+
+    const div = document.createElement('div');
+    div.classList.add('col')
+    div.innerHTML = `
+            <div class="card w-100 mx-auto">
+                <img src="${data.image}" class="card-img-top w-50 mx-auto" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${data.name}</h5>
+                    <h5 class="card-title">${data.releaseDate}</h5>
+
+ 
+                </div>
+            </div>
+    `
+    parent.appendChild(div)
+}
+
+
+
+
+
 
 
 // For showing result 
