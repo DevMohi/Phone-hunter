c1667c10454258997933475d874b32af67e6e64d
DevMohi <morphex72@gmail.com>
2022-02-28 22:50:03 +0600

    made responsive and data can be shown into UI on load

--- js/phone.js
+++ js/phone.js
@@ -1,1 +1,46 @@
-console.log('hello')
+const loadData = () => {
+
+    // getting input 
+    const inputText = document.getElementById('inputText').value;
+
+    // fetching data 
+    url = `https://openapi.programming-hero.com/api/phones?search=${inputText}`
+
+    fetch(url)
+        .then(res => res.json())
+        .then(phone => displayData(phone.data))
+}
+
+const displayData = data => {
+
+    const parent = document.getElementById('displaySection')
+    parent.textContent = '';
+
+    if (data.length == 0) {
+        console.log('error')
+    }
+
+    else {
+        data.forEach(phone => {
+            console.log(phone)
+            const div = document.createElement('div')
+            div.classList.add('col')
+            div.innerHTML = `
+            <div class="card w-75 mx-auto pt-3">
+                <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="no image found">
+                <div class="card-body">
+                <h5 class="card-title text-center">${phone.brand}</h5>
+                <h5 class="card-title text-center">${phone.phone_name}</h5>
+
+                <div class="d-flex justify-content-center border-none">
+                    <button class="border border-dark btn-danger py-1 px-2 ">Details</button>
+                </div>
+
+                </div>
+            </div>
+        `
+            parent.appendChild(div);
+        })
+    }
+
+}
