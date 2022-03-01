ff9bac4a538b336975645d53daf2240d380033d4
DevMohi <morphex72@gmail.com>
2022-03-01 13:58:44 +0600

    borders added and names added

--- js/phone.js
+++ js/phone.js
@@ -1,3 +1,5 @@
+// loading the initial data from api 
+
 const loadData = () => {
     // getting input 
     const inputText = document.getElementById('inputText').value;
@@ -5,7 +7,7 @@ const loadData = () => {
     document.getElementById('detailsSection').textContent = '';
     spinner('block');
 
-    // fetching data 
+    // fetching data, if there is no data it will show error
     if (inputText == "") {
         spinner('none')
         resultShow('block')
@@ -13,8 +15,6 @@ const loadData = () => {
 
     else {
         url = `https://openapi.programming-hero.com/api/phones?search=${inputText}`
-
-
         fetch(url)
             .then(res => res.json())
             .then(phone => displayData(phone.data))
@@ -22,11 +22,13 @@ const loadData = () => {
 
 }
 
-
+// displaying the details in ui 
 const displayData = data => {
     console.log(data)
     const parent = document.getElementById('displaySection');
     parent.textContent = '';
+
+// if the search input is not correct it will not show any phones 
 
     if (data.length == 0) {
         console.log('error');
@@ -47,7 +49,7 @@ const displayData = data => {
                 <h5 class="card-title text-center">${phone.phone_name}</h5>
 
                 <div class="d-flex justify-content-center border-none" >
-                    <button class="border btn-danger py-2 px-3 rounded" onclick="loadDetail('${phone.slug}')"><a class ="text-white" href="#detailsSection" style="text-decoration: none;">Details</a></button>
+                    <button class="border btn-dark py-2 px-3 rounded" onclick="loadDetail('${phone.slug}')"><a class ="text-white" href="#detailsSection" style="text-decoration: none;">Details</a></button>
                 </div>
 
                 </div>
@@ -61,7 +63,7 @@ const displayData = data => {
 
 }
 
-// getting the id and loading it 
+// using the id of the phone and loading more details 
 const loadDetail = id => {
 
     url = `https://openapi.programming-hero.com/api/phone/${id}`
@@ -71,7 +73,7 @@ const loadDetail = id => {
         .then(details => displayDetails(details.data))
 }
 
-// showing details in ui 
+// showing details in ui using the data got from id
 const displayDetails = data => {
 
     console.log(data);
@@ -81,6 +83,8 @@ const displayDetails = data => {
     const sensors = data.mainFeatures.sensors;
 
     const ul = document.createElement('ul')
+
+    //for looping the sensors and appending it directly to a ul and finally set it to a innerHTML
     for (const sensor of sensors) {
         const li = document.createElement('li')
         li.innerText = sensor;
@@ -92,49 +96,56 @@ const displayDetails = data => {
     const div = document.createElement('div');
     div.classList.add('col')
     div.innerHTML = `
-            <div class="card mb-3 py-3">
-                <div class="row">
+        <div class="card mb-3 py-3 rounded border-dark">
+            <div class="row">
                 <div class="col-md-4 d-flex justify-content-center">
                     <img src="${data.image}" class="" alt="...">
                 </div>
 
                 <div class="col-md-8 py-2">
                     <div class="displayCard py-1">
+                        <h1 class="card-title">${data.brand}</h1>
                         <h1 class="card-title">${data.name}</h1>
-                        <h5>${data.releaseDate ? data.releaseDate : "Release Date Not Found"}</h5> <br>
-                        <h2>Main Features</h2>
-                        <h5><span class = "fw-bold">storage</span>: ${data.mainFeatures.storage} </h5>
-                        <h5><span class = "fw-bold ">displaySize</span>: ${data.mainFeatures.displaySize} </h5> 
-                        <h5><span class = "fw-bold">chipSet</span>: ${data.mainFeatures.chipSet} </h5>
-                        <h5><span class = "fw-bold">memory</span>: ${data.mainFeatures.memory} </h5>
-                        <div class="dropdown py-2">
-                            <button class="btn btn-secondary  dropdown-toggle" type="button" id="dropdownMenuButton1"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                Sensor
-                            </button>
-                            <ul class="dropdown-menu ps-2 pe-2" aria-labelledby="dropdownMenuButton1">
-                                ${ul.innerHTML}
-                            </ul>
-                        </div>
-                        <div class="dropdown py-2 ">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                Others
-                            </button>
-                                <ul class="dropdown-menu ps-2 pe-2" aria-labelledby="dropdownMenuButton1 ">
-                                    <li>WLAN: ${data.others?.WLAN ? data.others.WLAN : "Not Found"} </li>
-                                    <li>BLUETOOTH: ${data.others?.Bluetooth ? data.others.Bluetooth : "Not Found"}</li>
-                                    <li>GPS: ${data.others?.GPS ? data.others.GPS : "Not Found"} </li>
-                                    <li>NFC: ${data.others?.NFC ? data.others.NFC : "Not Found"} </li>
-                                    <li>Radio: ${data.others?.Radio ? data.others.Radio : "Not Found"} </li>
-                                    <li>USB: ${data.others?.USB ? data.others.USB : "Not Found"}</li>
-                                </ul>
+                        <h5 class = "fw-bold">${data.releaseDate ? data.releaseDate : "Release Date Not Found"}</h5> <br>
+
+                         <div class = "main-section">
+                            <h2>Main Features</h2>
+                            <h5><span class = "fw-bold">storage</span>: ${data.mainFeatures.storage} </h5>
+                            <h5><span class = "fw-bold ">displaySize</span>: ${data.mainFeatures.displaySize} </h5> 
+                            <h5><span class = "fw-bold">chipSet</span>: ${data.mainFeatures.chipSet} </h5>
+                            <h5><span class = "fw-bold">memory</span>: ${data.mainFeatures.memory} </h5>
+                            <div class = "d-flex dropdownSection">
+                                <div class="dropdown py-2">
+                                    <button class="btn btn-dark dropdown-toggle" type="button" id="dropdownMenuButton1"
+                                        data-bs-toggle="dropdown" aria-expanded="false">
+                                        Sensor
+                                    </button>
+                                    <ul class="dropdown-menu ps-2 pe-2" aria-labelledby="dropdownMenuButton1">
+                                        ${ul.innerHTML}
+                                    </ul>
+                                    
+                                </div>
+                                <div class="dropdown py-2 mx-2">
+                                    <button class="btn btn-dark dropdown-toggle" type="button" id="dropdownMenuButton1"
+                                        data-bs-toggle="dropdown" aria-expanded="false">
+                                        Others
+                                    </button>
+                                        <ul class="dropdown-menu ps-2 pe-2" aria-labelledby="dropdownMenuButton1 ">
+                                            <li>WLAN: ${data.others?.WLAN ? data.others.WLAN : "Not Found"} </li>
+                                            <li>BLUETOOTH: ${data.others?.Bluetooth ? data.others.Bluetooth : "Not Found"}</li>
+                                            <li>GPS: ${data.others?.GPS ? data.others.GPS : "Not Found"} </li>
+                                            <li>NFC: ${data.others?.NFC ? data.others.NFC : "Not Found"} </li>
+                                            <li>Radio: ${data.others?.Radio ? data.others.Radio : "Not Found"} </li>
+                                            <li>USB: ${data.others?.USB ? data.others.USB : "Not Found"}</li>
+                                        </ul>
+                                </div>
+                         </div>
                         </div>
                     </div>
 
-                    </div>
                 </div>
             </div>
+        </div>
 
     
         `
